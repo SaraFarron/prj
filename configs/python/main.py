f26a2072f9cb977902e50699dd9875fdbd652cba
NikitaSiulin <n.siulin@rufago.ru>
2023-12-12 00:49:23 +0700

    change python snippet, added templates

---
+++ configs/python/main.py
@@ -0,0 +1,8 @@
+from const import *
+from utils import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
--- configs/scripts/python-app.sh
+++ configs/scripts/python-app.sh
@@ -2,19 +2,11 @@
 
 source "$PRJ_PATH/const.sh"
 source "$CONFIGS/python.sh"
+PYTHON_CONFIGS="$CONFIGS/python"
 
 curl-gitignore
 init-pipenv
 create-main-py
 touch const.py
 touch utils.py
-echo "from const import *" >> main.py
-echo "from utils import *" >> main.py
-echo "" >> main.py
-echo "def main()" >> main.py
-echo "    pass" >> main.py
-echo "" >> main.py
-echo "" >> main.py
-echo "if __name__ == \"__main__\"" >> main.py
-echo "    main()" >> main.py
-echo "" >> main.py
+cat "$PYTHON_CONFIGS/main.py" >> main.py
